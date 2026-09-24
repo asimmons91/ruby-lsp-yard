9fa84d5d0af527cdab2d59d63066bf09fef38512
Austin Simmons <austin_simmons@fastmail.com>
2026-09-24 06:09:09 -0700

    initial commit

---
+++ lib/ruby/lsp/yard.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "yard/version"
+
+module Ruby
+  module Lsp
+    module Yard
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/lsp/yard/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Ruby
+  module Lsp
+    module Yard
+      VERSION = "0.1.0"
+    end
+  end
+end
